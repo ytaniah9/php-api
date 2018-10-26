31067ef063e8589f30859b1d6dbadfc0aa2fdc53
ytaniah9 <ytaniah9@gmail.com>
2018-10-26 07:48:27 +0700

    php api

--- koneksi.php
+++ koneksi.php
@@ -7,5 +7,5 @@ $conn = new mysqli($servername,$username,$password,$database);
 if($conn->connect_error){
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connection Succes";
+echo " ";
  ?>
